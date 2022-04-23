2dc04f3cd0aa6acc02ff72c41f484b128c8f0f67
Promise Johnson <chiemelapromise30@gmail.com>
2022-04-23 22:52:19 +0100

    Deployment

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const BASE_URL = process.env.REACT_APP_PUBLIC_URL
 // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYTFmZDFmY2RmZTEzOTA0NWM5YjY3MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzODM1NzE2NiwiZXhwIjoxNjM4NjE2MzY2fQ.Mz9_PfRzTPgQikwrtOsSJeI9nkJNkMqD5Slv_2KbQD8"
